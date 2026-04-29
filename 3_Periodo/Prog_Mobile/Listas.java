529f9e7381864a8d6df83cc4092d64999fb7abe3
Artorias <240697617+ArthurFNCorreia64@users.noreply.github.com>
2026-04-29 12:24:12 +0000

    fixes

--- 3_Periodo/Prog_Mobile/Listas.java
+++ 3_Periodo/Prog_Mobile/Listas.java
@@ -8,7 +8,7 @@ public class Listas {
         // prepara a leitura de dados do teclado
         Scanner entrada = new Scanner(System.in);
         // cria uma lista onde os números serão guardados
-        ArrayList<Interger> lista = new ArrayList<>();
+        ArrayList<Integer> lista = new ArrayList<>();
         // inicializa a variável que guarda o valor digitado
         int numero =-1;
 
@@ -16,7 +16,7 @@ public class Listas {
         while (numero !=0){
             System.out.print("Digite um número (0 para sair): ");
             // lê o texto digitado e transforma em número
-            numero = Interger.parseInt(entrada.nextLine());
+            numero = Integer.parseInt(entrada.nextLine());
             // só adiciona à lista se não for o valor de saída
             if (numero != 0){
                 lista.add(numero);
